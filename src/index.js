9eeb5c5cd31d11b3f4e08cc42602b35351dcd55c
yomoda07 <kanji.yy@gmail.com>
2017-10-03 00:06:04 -0700

    Set up basic routes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './styles/index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const ExpenseDashboardPage = () => (
+  <div>
+    This is from my dashboard component
+  </div>
+);
+
+const AddExpensePage = () => (
+  <div>
+    This is from my dashboard component
+  </div>
+);
+
+const EditExpensePage = () => (
+  <div>
+    This is from my edit component
+  </div>
+);
+
+const HelpPage = () => (
+  <div>
+    This is from my help component
+  </div>
+);
+
+
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={ExpenseDashboardPage} exact={true} />
+      <Route path="/create" component={AddExpensePage} />
+      <Route path="/edit" component={EditExpensePage} />
+      <Route path="/help" component={HelpPage} />
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('root'));
 registerServiceWorker();
